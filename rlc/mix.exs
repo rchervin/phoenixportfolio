147d5d20ceccded80ba0b5532254af0726283319
Rachel Chervin <lanagan.inc@gmail.com>
2016-12-31 10:12:28 -0800

    add haml and remove phoenix css

--- rlc/mix.exs
+++ rlc/mix.exs
@@ -37,7 +37,8 @@ defmodule Rlc.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:phoenix_haml, github: "chrismccord/phoenix_haml"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
